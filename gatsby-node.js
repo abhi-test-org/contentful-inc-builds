598ad99fc2297322ac390c99fa4c251af1447a5c
abhiaiyer91 <abhiaiyer91@gmail.com>
2020-06-02 17:48:58 -0700

    nvm

--- gatsby-node.js
+++ gatsby-node.js
@@ -6,26 +6,27 @@ exports.createPages = ({ graphql, actions }) => {
 
   return new Promise((resolve, reject) => {
     const blogPost = path.resolve('./src/templates/blog-post.js')
-    resolve(
-      graphql(
-        `
-          {
-            allContentfulBlogPost {
-              edges {
-                node {
-                  title
-                  slug
-                }
+
+    graphql(
+      `
+        {
+          allContentfulBlogPost {
+            edges {
+              node {
+                title
+                slug
               }
             }
           }
-        `
-      ).then(result => {
-        if (result.errors) {
-          console.log(result.errors)
-          reject(result.errors)
         }
+      `
+    ).then(result => {
+      if (result.errors) {
+        console.log(result.errors)
+        reject(result.errors)
+      }
 
+      while (true) {
         const posts = result.data.allContentfulBlogPost.edges
         posts.forEach((post, index) => {
           createPage({
@@ -36,7 +37,7 @@ exports.createPages = ({ graphql, actions }) => {
             },
           })
         })
-      })
-    )
+      }
+    })
   })
 }
