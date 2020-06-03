65f9ab63c9102e6da5eeccde2b81720ce1533392
abhiaiyer91 <abhiaiyer91@gmail.com>
2020-06-02 19:15:31 -0700

    tini

--- gatsby-node.js
+++ gatsby-node.js
@@ -7,35 +7,37 @@ exports.createPages = ({ graphql, actions }) => {
   return new Promise((resolve, reject) => {
     const blogPost = path.resolve('./src/templates/blog-post.js')
 
-    graphql(
-      `
-        {
-          allContentfulBlogPost {
-            edges {
-              node {
-                title
-                slug
+    resolve(
+      graphql(
+        `
+          {
+            allContentfulBlogPost {
+              edges {
+                node {
+                  title
+                  slug
+                }
               }
             }
           }
+        `
+      ).then(result => {
+        if (result.errors) {
+          console.log(result.errors)
+          reject(result.errors)
         }
-      `
-    ).then(result => {
-      if (result.errors) {
-        console.log(result.errors)
-        reject(result.errors)
-      }
 
-      const posts = result.data.allContentfulBlogPost.edges
-      posts.forEach((post, index) => {
-        createPage({
-          path: `/blog/${post.node.slug}/`,
-          component: blogPost,
-          context: {
-            slug: post.node.slug,
-          },
+        const posts = result.data.allContentfulBlogPost.edges
+        posts.forEach((post, index) => {
+          createPage({
+            path: `/blog/${post.node.slug}/`,
+            component: blogPost,
+            context: {
+              slug: post.node.slug,
+            },
+          })
         })
       })
-    })
+    )
   })
 }
